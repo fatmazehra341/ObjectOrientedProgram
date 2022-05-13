f8b4a1a4f7dac91f1433b2817d091932b181d14a
fatmazehra341 <fatmazehra341@gmail.com>
2022-05-13 17:48:00 +0530

    [Fatma] add:Data management added

---
+++ OOPS/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using OOPS;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        const string Path = @"C:\Users\Khadeeja Zehra\OneDrive\Desktop\Bridgelab\OOPS\OOPS\Inventory.json";
+        InventoryManagement inventoryManagement = new InventoryManagement();
+        bool end = true;
+        Console.WriteLine("1.Display Inventory");
+        while (end)
+        {
+            Console.WriteLine("Take an option to execute");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                inventoryManagement.DisplayInventory(Path);
+                    break;
+            }
+        }
+    }
+}
